2b0d434126d3e2bfa434de2bcd40723edd27b94b
mquarne <mquarne@nellisauction.com>
2023-04-21 16:49:21 -0700

    added title to head

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <title>Q's Remix Notes App</title>
       </head>
       <body>
         <Outlet />
